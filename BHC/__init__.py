4674c55995f10cc1415161a56e43c2e510e881f7
Peter Kim <minchel93@Peter-Kims-MBP.wired.duke.local>
2019-04-30 19:04:24 -0400

    Loglikelihood

--- BHC/__init__.py
+++ BHC/__init__.py
@@ -3,4 +3,4 @@ Bayesian Hierarchical Clustering (Heller and Ghahramani, 2005)
 """
 
 from .BHC import BHC
-from .Likelihood import Multinorm_post_marginal_likelihood
+from .Likelihood import log_Multinorm_post_marginal_likelihood
