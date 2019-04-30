c88b13b3879a73212da0a57beb52616f863ab7b7
Peter Kim <minchel93@Peter-Kims-MBP.wired.duke.local>
2019-04-30 13:36:17 -0400

    Updating __init__py.file

--- EP_BHC/EP_BHC/__init__.py
+++ EP_BHC/EP_BHC/__init__.py
@@ -2,5 +2,5 @@
 Bayesian Hierarchical Clustering (Heller and Ghahramani, 2005)
 """"""""
 
-from .EP_BHC import BHC
-from .EP_BHC import Multinorm_post_marginal_likelihood
+from .BHC import BHC
+from .likelihood import Multinorm_post_marginal_likelihood
