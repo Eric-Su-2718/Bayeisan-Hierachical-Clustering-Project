506d2b1d20e865cfb1878a6bb95e75972a80a964
Peter Kim <minchel93@Peter-Kims-MBP.wired.duke.local>
2019-04-30 19:23:30 -0400

    Log_likelihood

--- BHC/Likelihood.py
+++ BHC/Likelihood.py
@@ -1,13 +1,13 @@
 from scipy.special import multigammaln
 import numpy as np
 
-def Multinorm_post_marginal_likelihood(mu_0, kappa_0, nu_0, Psi_0):
+def log_Multinorm_post_marginal_likelihood(mu_0, kappa_0, nu_0, Psi_0):
     """
     Returns a function that computes the marginal likelihood 
     for a Normal-inverse-Wishart model with specifies data and prior parameters.
     """
         
-    def marginal_likelihood(data):
+    def log_marginal_likelihood(data):
         
         n = data.shape[0]
         p = data.shape[1]
@@ -24,7 +24,7 @@ def Multinorm_post_marginal_likelihood(mu_0, kappa_0, nu_0, Psi_0):
             C = (n - 1) * np.cov(data.T)
         Psi_n = Psi_0 + C + ((n * kappa_0) / (kappa_n)) * np.dot((xbar - mu_0).reshape(-1, p).T, (xbar - mu_0).reshape(-1, p))
     
-        return (1 / np.pi**(n * p / 2)) * (np.exp(multigammaln(nu_n / 2, p)) / np.exp(multigammaln(nu_0 / 2, p))) * (np.linalg.det(Psi_0)**(nu_0 / 2) / np.linalg.det(Psi_n)**(nu_n / 2)) * (kappa_0 / kappa_n)**(p / 2)
+        return -(n * p / 2) * (np.log(np.pi)) + multigammaln(nu_n / 2, p) - multigammaln(nu_0 / 2, p)) + (nu_0 / 2) * np.log(np.abs(np.linalg.det(Psi_0)**)) - (nu_n / 2) * np.log(np.abs(np.linalg.det(Psi_n)))) + (p / 2) * (np.log(kappa_0) - np.log(kappa_n))
 
-    return marginal_likelihood
+    return log_marginal_likelihood
 
