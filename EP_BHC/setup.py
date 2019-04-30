588f63a6a28095009a7e5e08fa943b32ba81f1e7
Peter Kim <minchel93@Peter-Kims-MBP.wired.duke.local>
2019-04-30 13:13:24 -0400

    Updating the version of setup.py

--- EP_BHC/setup.py
+++ EP_BHC/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = "EP_BHC",
     package = ["EP_BHC"]
-    version = "1.0",
+    version = "1.1",
     license = "MIT",
     description = "A Python package to generate Bayesian hierarchical clusters to a supplied data",
     authors = "Eric Su and Min Chul Kim",
